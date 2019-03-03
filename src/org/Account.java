9a804d08dc939f18b441a7c71408f8f975f4b23d
mrMouse <ia@asygnata.com>
2019-03-03 01:26:36 +0100

    finished implementation of nominals quantity: -check quantity during withdraw

--- src/org/Account.java
+++ src/org/Account.java
@@ -3,6 +3,7 @@ package org;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -10,7 +11,7 @@ public class Account {
     private BigDecimal balance;
 
     //map containing quantity of ech nominal, will be updated during deposit and withdraw
-    private Map<BigDecimal, Integer> nominalsQuantity = new LinkedHashMap<>();
+    private static Map<BigDecimal, Integer> nominalsQuantity = new LinkedHashMap<>();
 
     public Account(BigDecimal balance) {
         this.balance = balance;
@@ -40,22 +41,22 @@ public class Account {
     };
 
     //show quantity of certain nominal
-    private Integer getNominalsQuantity(BigDecimal nominal){
+    private Integer getNominalsQuantity(BigDecimal nominal) {
         return nominalsQuantity.get(nominal);
     }
 
     //update quantity of certain nominal, when subtracting Integer will be negative
-    private void setNominalsQuantity(BigDecimal nominal, Integer quantity){
-        nominalsQuantity.put(nominal, nominalsQuantity.get(nominal)+quantity);
+    private void setNominalsQuantity(BigDecimal nominal, Integer quantity) {
+        nominalsQuantity.put(nominal, nominalsQuantity.get(nominal) + quantity);
     }
 
     protected BigDecimal getBalance() {
         return balance;
     }
 
-    protected void getAllNominalsQuantity(){
+    protected void getAllNominalsQuantity() {
         System.out.println("Available nominal:");
-        for (Map.Entry<BigDecimal, Integer> entry :nominalsQuantity.entrySet()){
+        for (Map.Entry<BigDecimal, Integer> entry : nominalsQuantity.entrySet()) {
             StringBuilder msg = new StringBuilder();
             msg.append(entry.getValue());
             msg.append(" x ");
@@ -70,9 +71,10 @@ public class Account {
     }
 
     protected void withdraw(BigDecimal amount) {
-        if (amount.compareTo(balance) == -1 || amount.compareTo(balance) == 0) {
-            balance = balance.subtract(amount);
-            System.out.println(value(amount));
+        if (amount.compareTo(balance) <= 0) {
+            if(value(amount)){
+                balance= balance.subtract(amount);
+            }
         } else {
             System.out.println("your too poor!");
         }
@@ -87,15 +89,54 @@ public class Account {
         return false;
     }
 
-    private static String value(BigDecimal value) {
-        String ret = "Amount " + value + "\n";
-        for (BigDecimal nominal : NOMINALS) {
-            if (value.compareTo(nominal) >= 0) {
-                BigDecimal a = value.divide(nominal, 0, RoundingMode.FLOOR);
-                value = value.subtract(nominal.multiply(a));
-                ret += "" + a + " x " + nominal + "\n";
+//    private static String value(BigDecimal value) {
+//        String ret = "Amount " + value + "\n";
+//        for (BigDecimal nominal : NOMINALS) {
+//            if (value.compareTo(nominal) >= 0) {
+//                BigDecimal a = value.divide(nominal, 0, RoundingMode.FLOOR);
+//                value = value.subtract(nominal.multiply(a));
+//                ret += "" + a + " x " + nominal + "\n";
+//            }
+//        }
+//        return ret;
+//    }
+
+    private boolean value(BigDecimal value) {
+        StringBuilder print = new StringBuilder();
+        Map<BigDecimal, Integer> tempMap = new HashMap<>();
+        for (Map.Entry<BigDecimal, Integer> nominal : nominalsQuantity.entrySet()) {
+            if (nominal.getValue() > 0) {
+                if (value.compareTo(nominal.getKey()) >= 0) {
+                    //what we need
+                    BigDecimal a = value.divide(nominal.getKey(), 0, RoundingMode.FLOOR);
+                    //what we have
+                    BigDecimal b = BigDecimal.valueOf(new Double(nominal.getValue()));
+                    if (a.compareTo(b) == 1) {
+                        value = value.subtract(nominal.getKey().multiply(b));
+                        print.append(b);
+                        print.append(" x ");
+                        print.append(nominal.getKey());
+                        print.append("\n");
+                        tempMap.put(nominal.getKey(),nominal.getValue());
+                    } else {
+                        value = value.subtract(nominal.getKey().multiply(a));
+                        print.append(a);
+                        print.append(" x ");
+                        print.append(nominal.getKey());
+                        print.append("\n");
+                        tempMap.put(nominal.getKey(), new Integer(a.toString()));
+                    }
+                }
             }
         }
-        return ret;
+        if (value.compareTo(BigDecimal.ZERO) != 0) {
+            System.out.println("Operation denied - no suitable nominal");
+            return false;
+        }
+        System.out.println(print);
+        for (Map.Entry<BigDecimal, Integer> nominal : tempMap.entrySet()){
+            this.setNominalsQuantity(nominal.getKey(), -nominal.getValue());
+        }
+        return true;
     }
 }
